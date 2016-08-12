1bfbd6a35382d3ee42e51d821539c4247dccd427
KSJ <smart23033@gmail.com>
2016-08-12 17:17:08 +0900

    Initial commit

---
+++ app/src/main/java/example/tacademy/com/samplemedia/MainActivity.java
@@ -0,0 +1,13 @@
+package example.tacademy.com.samplemedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
